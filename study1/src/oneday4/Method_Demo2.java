c742359caa3a1dedaa23e0fb1d9b08b80231aa38
kaiqi <zhang20170730@gmail.com>
2022-11-03 20:31:08 +0800

    方法

--- study1/src/oneday4/Method_Demo2.java
+++ study1/src/oneday4/Method_Demo2.java
@@ -6,18 +6,20 @@ import org.omg.CosNaming.BindingIterator;
 * 有返回值和无返回值的对比*/
 public class Method_Demo2 {
     public static void main(String[] args) {
-        int num = getsum(10, 20);
+
+        getsum(101,200);  //单独调用
+        int num = getsum(10, 20); // 赋值调用
+        System.out.println("打印调用："+ getsum(10, 200)); // 打印调用
         System.out.println("返回值是："+num);
         System.out.println("==========");
-        printsum(100,202);
 
 
+        printsum(100,202);  // 单独调用
         //有返回值的方法可以单独调用，打印调用，或者赋值调用
         //无返回值的方法，只能使用单独调用不能使用打印或赋值调用
 //        System.out.println(printsum(););
 //        int num1 = printsum(100,200);
 //        System.out.println(num1);
-
 
     }
 
