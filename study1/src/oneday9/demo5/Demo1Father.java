586bb31b5c6389c4b6e476026d3f3664b046f6e7
kaiqi <zhang20170730@gmail.com>
2022-11-23 22:14:54 +0800

    继承三个特点

---
+++ study1/src/oneday9/demo5/Demo1Father.java
@@ -0,0 +1,9 @@
+package oneday9.demo5;
+
+public class Demo1Father {
+    int num = 100;
+    public void method(){
+        System.out.println("父类方法");
+    }
+
+}
---
+++ study1/src/oneday9/demo5/Demo1Son.java
@@ -0,0 +1,45 @@
+package oneday9.demo5;
+/*
+* this关键字的用法：
+* 1.在奔雷成员方法总访问本类成员变量
+* 2.在本类的成员方法中，访问本类的另一个成员方法
+* 3.在本来都构造方法中，访问本类的另一个构造方法
+* */
+public class Demo1Son extends Demo1Father {
+    int num = 200;
+    public Demo1Son(){
+
+        this(123);  // 本类中无参构造调用本类中有参构造；
+    }
+
+    public Demo1Son(int n ){
+
+    }
+
+    public Demo1Son(int n , int m){
+
+    }
+    public void showNum() {
+        int num = 10;
+        System.out.println(num);
+        System.out.println(this.num);
+        System.out.println(super.num);
+
+    }
+
+    public void methodA() {
+        System.out.println("AAA");
+    }
+
+
+    public void methodB(){
+        this.method();
+        System.out.println("BBB");
+    }
+
+    public void method(){
+        method();
+        super.method();
+    }
+
+}
