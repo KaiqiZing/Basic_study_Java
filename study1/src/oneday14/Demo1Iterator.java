6b4ba82512b136d7d343507980569c998445e298
KaiqiZing <zhang20170730@gmail.com>
2023-01-02 11:48:58 +0800

    Iterator方法

--- study1/src/oneday14/Demo1Iterator.java
+++ study1/src/oneday14/Demo1Iterator.java
@@ -23,6 +23,8 @@ import java.util.Iterator;
 public class Demo1Iterator {
     public static void main(String[] args) {
         InteratorDemo1();
+        Demo2();
+
 
     }
 
@@ -38,6 +40,29 @@ public class Demo1Iterator {
         for (Iterator<String> it2 = String_list.iterator(); it2.hasNext();){
             String e2 = it2.next();
             System.out.println("forIterator方法："+e2);
+        }
+    }
+
+
+
+    /*
+    增强for循环:底层使用的也是迭代器,使用for循环的格式,简化了迭代器的书写
+    是JDK1.5之后出现的新特性
+    Collection<E>extends Iterable<E>:所有的单列集合都可以使用增强for
+    public interface Iterable<T>实现这个接口允许对象成为 "foreach" 语句的目标。
+
+    增强for循环:用来遍历集合和数组
+
+    格式:
+        for(集合/数组的数据类型 变量名: 集合名/数组名){
+            sout(变量名);
+        }
+ */
+    private static void Demo2(){
+        ArrayList<String> String_list2 = new ArrayList<>();
+        Collections.addAll(String_list2, "abc2", "bbb2", "ccc2");
+        for (String s :String_list2){
+            System.out.println("增强for循环："+ s);
         }
     }
 
