623a314bb8214e3b484ac236852830358f8853e4
KaiqiZing <zhang20170730@gmail.com>
2023-01-04 22:36:39 +0800

    内部类

--- study1/src/oneday12/Demo2/Demo02InnerClass.java
+++ study1/src/oneday12/Demo2/Demo02InnerClass.java
@@ -1,4 +1,11 @@
 package oneday12.Demo2;
 
 public class Demo02InnerClass {
+    // 外部类名称.内部类名称 对象名 = new 外部类名称().new 内部类名称();
+
+    public static void main(String[] args) {
+        Outer.Inner obj = new Outer().new Inner();
+        obj.methodInner();
+
+    }
 }
--- study1/src/oneday12/Demo2/Outer.java
+++ study1/src/oneday12/Demo2/Outer.java
@@ -1,4 +1,18 @@
 package oneday12.Demo2;
-
+/*如果出现重名现象，那么格式是：外部类名称.this.外部类成员变量名
+* */
 public class Outer {
+    int num = 10;
+
+    public class Inner{
+        int num = 20;
+        public void methodInner(){
+            int num = 30;
+            System.out.println("局部变量就近原则："+num);
+            System.out.println("内部类成员变量："+ this.num);
+            System.out.println("外部类的成员变量:"+ Outer.this.num);
+
+
+        }
+    }
 }
