659653de8fad06c56c56c6c6322f21f88d0f5dc3
KaiqiZing <zhang20170730@gmail.com>
2023-01-08 17:30:58 +0800

    匿名内部类

---
+++ study1/src/oneday12/Demo3/MyInterfaceImpl.java
@@ -0,0 +1,16 @@
+package oneday12.Demo3;
+
+public class MyInterfaceImpl implements Myinterface{
+
+    @Override
+    public void method1() {
+        System.out.println("覆盖重写方法1");
+    }
+
+    @Override
+    public void method() {
+
+        System.out.println("覆盖重写方法2");
+
+    }
+}
